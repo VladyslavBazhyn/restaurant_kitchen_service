377a80318ede10d3e40dda07d799c051bd0c6dbd
Vlad <mr.darmstadtium@gmail.com>
2024-03-20 18:00:01 +0100

    add path to cook list, template cook_list, view class CookListView

---
+++ kitchen/urls.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from kitchen.views import (
+    index,
+    CookListView
+)
+
+urlpatterns = [
+    path("", index),
+    path(
+        "cooks/",
+        CookListView.as_view(),
+        name="cook-list"
+    )
+]
+
+app_name = "kitchen"

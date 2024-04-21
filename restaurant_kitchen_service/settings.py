7bfca3ebbba213a5a04b329bd590278bb40830c2
Vlad <mr.darmstadtium@gmail.com>
2024-04-21 11:22:21 +0200

    add some requirements.txt, rebind application to amazon free db

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -83,9 +83,13 @@ WSGI_APPLICATION = 'restaurant_kitchen_service.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "kitchen_db",
+        "USER": "gibbs",
+        "PASSWORD": "greATparol1!",
+        "HOST": "database-1.cr86koiou84h.eu-west-2.rds.amazonaws.com",
+        "PORT": "5432"
     }
 }
 
